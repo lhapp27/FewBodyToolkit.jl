9304f19188030ee184166abd965ed73d99b49a40
lhapp27 <lucas.happ@riken.jp>
2025-05-22 18:26:49 +0900

    package structure

--- src/FewBodyToolkit.jl
+++ src/FewBodyToolkit.jl
@@ -1,5 +1,22 @@
 module FewBodyToolkit
 
-greet() = print("Hello World!")
+### GEM-2body
+include("GEM-2body/GEM1D.jl")
+include("GEM-2body/GEM2D.jl")
+include("GEM-2body/GEM3D.jl")
+include("GEM-2body/GEMq1D.jl")
+
+using .GEM1D
+using .GEM2D
+using .GEM3D
+using .GEMq1D
+
+### GEM-3body-1D
+include("GEM-3body-1D/GEM3B1D.jl")
+using .GEM3B1D
+
+### ISGL-3body
+include("ISGL-3body/ISGL.jl")
+using .ISGL
 
 end # module FewBodyToolkit
